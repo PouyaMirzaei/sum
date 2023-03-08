02dc29113ecab7602ef0ac1748c0cd622b9fda75
Pouya <zsamoraei@yahoo.com>
2023-03-08 15:00:53 +0330

    Add project files.

---
+++ sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Exercise28
+{
+    public static void Main()
+    {
+        int i, j;
+        int[,] arr1 = new int[10, 10];
+        int det = 0;
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                Console.Write("element - [{0}],[{1}] : ", i, j);
+                arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.Write("The matrix is :\n");
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+                Console.Write("{0}  ", arr1[i, j]);
+            Console.Write("\n");
+        }
+        for (i = 0; i < 3; i++)
+            det = det + (arr1[0, i] * (arr1[1, (i + 1) % 3] * arr1[2, (i + 2) % 3] - arr1[1, (i + 2) % 3] * arr1[2, (i + 1) % 3]));
+        Console.Write(det);
+    }
+}
